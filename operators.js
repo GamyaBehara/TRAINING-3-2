cfb5ec345d703c99aa3215d319f9021ca463588f
Gamya <beherasrigamya@gmail.com>
2025-01-08 12:56:52 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,11 @@
+console.log(10+10);
+console.log("10"+10);
+console.log(10+20+"100");
+console.log("hi" + 10+20);
+console.log("hi" + (10+20));
+
+// assignment operators(=)
+let a=10;
+console.log(a+5);
+
+
