bb912ecdc898110af4b2f4fc3b268b2dbd0f0218
GAMYA BEHARA <152589726+GamyaBehara@users.noreply.github.com>
2025-01-24 15:14:20 +0530

    js file

---
+++ Assignments/event.js
@@ -0,0 +1,20 @@
+// Add event listeners for each text
+document.getElementById("Plum").addEventListener("mouseover", () => {
+    document.body.style.background = "Plum";
+});
+
+document.getElementById("coral").addEventListener("mouseover", () => {
+    document.body.style.background = "coral";
+});
+
+document.getElementById("beige").addEventListener("mouseover", () => {
+    document.body.style.background = "beige";
+});
+
+document.getElementById("violet").addEventListener("mouseover", () => {
+    document.body.style.background = "violet";
+});
+
+document.getElementById("LightSeaGreen").addEventListener("mouseover", () => {
+    document.body.style.background = "LightSeaGreen";
+});
